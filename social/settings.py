87928ef73381b8b04b461621f379c11f6e00e441
SD170 <duttasaswata7@gmail.com>
2020-05-16 11:08:47 +0530

    migtate

--- social/settings.py
+++ social/settings.py
@@ -127,6 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Here you tell django to look for a folder named 'assets'
 ]
